ed3d108e8d25641a3c165a867ca2c4305b8f0bc5
Martin Cooper <martin.cooper@me.com>
2014-12-18 17:57:51 +0000

    Updating DataRow.

--- src/main/scala/com/github/martincooper/datatable/DataRow.scala
+++ src/main/scala/com/github/martincooper/datatable/DataRow.scala
@@ -16,23 +16,49 @@
 
 package com.github.martincooper.datatable
 
+import scala.util.Try
+
 /** Allows access to the underlying data in a row format. */
-class DataRow(dataTable: DataTable) {
+class DataRow(dataTable: DataTable, index: Int) {
 
   val table = dataTable
+  val rowIndex = index
 
-  val columnNameMap = dataTable.columns.map(col => col.name -> col.data).toMap
+  /** Indexer using the column name. */
+  def apply(columnIndex: Int): Any = table.col(columnIndex).data(rowIndex)
 
-  /** Returns all the values for a row at the specified index. */
-  def values(index: Int): IndexedSeq[Any] = {
-    dataTable.columns.map(_.data(index))
+  /** Indexer using the column index. */
+  def apply(columnName: String): Any = table.col(columnName).data(rowIndex)
+
+  /** Gets the column by index, as Option in case it doesn't exist. */
+  def get(columnIndex: Int): Option[Any] = colToValue(table.getCol(columnIndex))
+
+  /** Gets the column by name, as Option in case it doesn't exist. */
+  def get(columnName: String): Option[Any] = colToValue(table.getCol(columnName))
+
+  /** Gets the typed column by index. */
+  def as[T](columnIndex: Int): T = table.col(columnIndex).data(rowIndex).asInstanceOf[T]
+
+  /** Gets the typed column by name. */
+  def as[T](columnName: String): T = table.col(columnName).data(rowIndex).asInstanceOf[T]
+
+  /** Gets the typed column by index, as Option in case it doesn't exist or invalid type. */
+  def getAs[T](columnIndex: Int): Option[T] = colToTypedValue(table.getCol(columnIndex))
+
+  /** Gets the typed column by name, as Option in case it doesn't exist or invalid type. */
+  def getAs[T](columnName: String): Option[T] = colToTypedValue(table.getCol(columnName))
+
+  private def colToValue(column: Option[GenericColumn]): Option[Any] = {
+    column match {
+      case Some(col) => Try(col.data(rowIndex)).toOption
+      case _ => None
+    }
   }
 
-  def value(columnName: String, index: Int) = {
-    columnNameMap(columnName)(index)
-  }
-
-  def valueOf[T](columnName: String, index: Int): T = {
-    columnNameMap(columnName)(index).asInstanceOf[T]
+  private def colToTypedValue[T](column: Option[GenericColumn]): Option[T] = {
+    column match {
+      case Some(col) => Try(col.data(rowIndex).asInstanceOf[T]).toOption
+      case _ => None
+    }
   }
 }
