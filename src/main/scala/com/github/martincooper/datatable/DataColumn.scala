c541fb29ef6260376b2659b9a933f401520c6fd0
Brian Roberts <brian.roberts@timgroup.com>
2014-12-26 00:50:09 -0500

    Use Try.map and .flatMap to avoid boilerplate.

--- src/main/scala/com/github/martincooper/datatable/DataColumn.scala
+++ src/main/scala/com/github/martincooper/datatable/DataColumn.scala
@@ -37,31 +37,22 @@ class DataColumn[T: TypeTag](columnName: String, columnData: Iterable[T]) extend
 
   /** Returns a new DataColumn[T] with the value added at the end. */
   def add[V: TypeTag](value: V): Try[GenericColumn] = {
-    validateType[V](value) match {
-      case false => Failure(DataTableException("Invalid value type on add."))
-      case true =>
-        val typedValue = value.asInstanceOf[T]
-        Success(new DataColumn[T](name, data :+ typedValue))
+    validateType[V]("add", value).map { typedValue =>
+      new DataColumn[T](name, data :+ typedValue)
     }
   }
 
   /** Returns a new DataColumn[T] with the value inserted at the specified index. */
   def insert[V: TypeTag](index: Int, value: V): Try[GenericColumn] = {
-    validateType[V](value) match {
-      case false => Failure(DataTableException("Invalid value type on insert."))
-      case true =>
-        val typedValue = value.asInstanceOf[T]
-        checkAndCreateNewColumn(() => VectorExtensions.insertItem(data, index, typedValue))
+    validateType[V]("insert", value).flatMap { typedValue =>
+      checkAndCreateNewColumn(() => VectorExtensions.insertItem(data, index, typedValue))
     }
   }
 
   /** Returns a new DataColumn[T] with the new value at the specified index. */
   def replace[V: TypeTag](index: Int, value: V): Try[GenericColumn] = {
-    validateType[V](value) match {
-      case false => Failure(DataTableException("Invalid value type on replace."))
-      case true =>
-        val typedValue = value.asInstanceOf[T]
-        checkAndCreateNewColumn(() => VectorExtensions.replaceItem(data, index, typedValue))
+    validateType[V]("replace", value).flatMap { typedValue =>
+      checkAndCreateNewColumn(() => VectorExtensions.replaceItem(data, index, typedValue))
     }
   }
 
@@ -71,14 +62,15 @@ class DataColumn[T: TypeTag](columnName: String, columnData: Iterable[T]) extend
   }
 
   private def checkAndCreateNewColumn(transformData: () => Try[Vector[T]]): Try[DataColumn[T]] = {
-    transformData() match {
-      case Success(modifiedData) => Success(new DataColumn[T](name, modifiedData))
-      case Failure(ex) => Failure(ex)
-    }
+    transformData().map { modifiedData => new DataColumn[T](name, modifiedData) }
   }
 
-  private def validateType[V: TypeTag](value: V): Boolean = {
-    if (typeOf[V] =:= columnType) true else false
+  private def validateType[V: TypeTag](reason: String, value: V): Try[T] = {
+    if (typeOf[V] =:= columnType) {
+      Success(value.asInstanceOf[T])
+    } else {
+      Failure(DataTableException(s"Invalid value type on ${reason}."))
+    }
   }
 
   override def toString = "Col : " + name
