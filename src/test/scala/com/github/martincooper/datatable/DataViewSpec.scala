cb7f67bbe19a431048550fa8c244722d16874064
Martin Cooper <martin.cooper@me.com>
2014-12-27 18:06:56 +0000

    Adding DataView unit tests.

--- src/test/scala/com/github/martincooper/datatable/DataViewSpec.scala
+++ src/test/scala/com/github/martincooper/datatable/DataViewSpec.scala
@@ -20,6 +20,39 @@ import org.scalatest.{ Matchers, FlatSpec }
 
 class DataViewSpec extends FlatSpec with Matchers {
 
-  // TODO
+  private def buildTestTable(): DataTable = {
+    val dataColOne = new DataColumn[Int]("ColOne", (0 to 10) map { i => i })
+    val dataColTwo = new DataColumn[String]("ColTwo", (0 to 10) map { i => "Value : " + i })
+    val dataColThree = new DataColumn[Boolean]("ColThree", (0 to 10) map { i => true })
 
+    DataTable("TestTable", Seq(dataColOne, dataColTwo, dataColThree)).get
+  }
+
+  "A new DataView" can "be created with valid data rows" in {
+    val dataTable = buildTestTable()
+
+    val dataView = DataView(dataTable, dataTable.take(5))
+
+    dataView.isSuccess should be(true)
+    dataView.get.length should be(5)
+  }
+
+  "A new DataView" can "be created with no data rows" in {
+    val dataTable = buildTestTable()
+
+    val dataView = DataView(dataTable, Seq())
+
+    dataView.isSuccess should be(true)
+    dataView.get.length should be(0)
+  }
+
+  "A new DataView" should "fail if table different to row table reference" in {
+    val dataTable = buildTestTable()
+
+    val dataRows = dataTable.take(5)
+    val dataView = DataView(DataTable("DifferentTable").get, dataRows)
+
+    dataView.isSuccess should be(false)
+    dataView.failed.get.getMessage should be("DataRows do not all belong to the specified table.")
+  }
 }
